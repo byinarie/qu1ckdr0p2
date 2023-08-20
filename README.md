a1455bfe6424723227fbd34b4114c307b8a1ec09
byinarie <byinarie@gmail.com>
2023-08-20 05:02:18 -0500

    v1.0.0

---
+++ README.md
@@ -0,0 +1,107 @@
+
+<h1 align="center">
+    <img src="img/quick_drop_small.png"</img>
+</h1>
+
+<h4 align="center">Rapidly host payloads and post exploitation bins over http or https.</h4>
+<h6 align="center">Designed to be used on exams like OSCP / PNPT or CTFs HTB / etc.</h4>
+<h6 align="center">Pull requests and issues welcome. As are any contributions.</h4>
+<h6 align="center">Batteries included.</h4>
+
+
+<p align="center">
+  <a href="#install">Install</a> •
+  <a href="#install">About</a> •
+  <a href="#examples">Examples</a> •
+  <a href="#license">License</a>
+</p>
+
+## About
+Host files fast using aliases, specify a directory (-d), or specify a file (-f). Set serv.py as an alias to use it anywhere. Supports hosting over http or https. If https is chosen it will generate a certificate and use it automatically. The interface is chosen by priority, if tun0 is up then tun0 will be used, otherwise it will use eth0. Download tooling to serve or set your own aliases using the ini files. Downloads require a github API token.
+
+Qu1ckdr0p comes with some tools in windows/ and linux/ by default, along with some default repos in config/repos.ini. Not all of the default tools have compiled binaries released in their source code repos (e.g: ghostpack) so compiled ones are included without the release repo added. Running `serv.py update` (requires github api key) will reach out to the repos configured in config/repos.ini, download them, and move them into the correct platform directory. All of the tools in the qu1ckdr0p repo and all of the Kali default tools (or most of them) also have a built in alias.
+
+## Install
+```zsh
+git clone https://github.com/byinarie/qu1ckdr0p.git /opt/qu1ckdr0p
+cd /opt/qu1ckdr0p/ 
+poetry install
+alias serv="python3 /opt/qu1ckdr0p/serv.py" (or add to ~/.zshrc)
+```
+
+## Examples 
+
+### Basic usage
+#### Defaults to http:80
+
+```zsh
+serv.py serve -d /path 
+serv.py serve -d /path --https --port 443
+serv.py serve -f /path/file.bin --https --port 443
+serv.py serve linux --https --port 443
+serv.py serve windows --https --port 443
+serv.py serve seatbelt --https --port 443
+serv.py serve mimikatz --https --port 443
+serv.py list --search "mimikatz"
+serv.py list
+```
+
+### Serve a windows privledge escalation directory over HTTPS 8443
+
+```zsh
+serv.py serve windows --https --port 8443
+# Pre configured post windows directory is: ./windows = /windows
+# see: config/common.ini
+```
+<p><img src="img/serve_windows.jpg"</img>
+
+### Serve the working directory
+
+```zsh
+serv.py serve 
+```
+<p><img src="img/working_dir.jpg"</img></p>
+
+### List currently configured aliases and search for a specific bin
+
+```zsh
+serv.py serve list --search winpeas
+serv.py serve list 
+
+```
+
+<p><img src="img/list_search_winpeas.jpg"</img>
+
+
+### Serve a single file over HTTPS 8443
+
+```zsh
+serv.py serve -f /usr/share/windows-resources/binaries/nc.exe --https --port 8443
+```
+<p><img src="img/serv_single_file.jpg"</img><p>
+
+
+### Serve a directory over default
+
+```zsh
+serv.py serve -d /usr/share/windows-resources/binaries
+```
+<p><img src="img/serve_directory.jpg"</img></p>
+
+### Download default tools
+#### First add your github api token to config/settings.ini
+#### Modify config/repos.ini as needed
+
+```zsh
+serv.py update
+```
+<p><img src="img/update.jpg"</img></p>
+
+
+
+## License
+
+MIT
+
+---
+
---
+++ serv.py
@@ -0,0 +1,300 @@
+from flask import Flask, send_from_directory
+from tqdm import tqdm
+from OpenSSL import crypto
+import os
+import netifaces as ni
+import logging
+import click
+import configparser
+import requests
+import zipfile
+import shutil
+import subprocess
+
+log = logging.getLogger('werkzeug')
+log.setLevel(logging.ERROR)
+config = configparser.ConfigParser()
+script_dir = os.path.dirname(os.path.abspath(__file__))
+common_ini_path = os.path.join(script_dir, 'config/common.ini')
+config.read(common_ini_path)
+# print(config.sections())
+# print(config.items("._DIR"))
+# Not sorting out other platforms for now
+blacklist_keywords = ['sample', 'arm64', 'readme.md', 'readme', 'license', 'mips', 'mips64', 'mipsle', 'ppc', 's390', 'arm', 'aarch64', 'armv7', 'armv8', 'armv6', 'armv5', 'armv4', 'armv3', 'armv2', 'armv1', 'armv0', 'armv', 'arm64', 'armle', 'armbe', 'armhf', 'armel', 'arm64', 'armv7', 'armv8', 'armv6', 'armv5', 'armv4', 'armv3', 'armv2', 'armv1', 'armv0', 'armv', 'arm64', 'armle', 'armbe', 'armhf', 'armel', 'arm64', 'armv7', 'armv8', 'armv6', 'armv5', 'armv4', 'armv3', 'armv2', 'armv1', 'armv0', 'armv', 'arm64', 'armle', 'armbe', 'armhf', 'armel', 'arm64', 'armv7', 'armv8', 'armv6', 'armv5', 'armv4', 'armv3', 'armv2', 'armv1', 'armv0', 'armv', 'arm64', 'armle', 'armbe', 'armhf', 'armel', 'arm64', 'armv7', 'armv8', 'armv6', 'armv5', 'armv4', 'armv3', 'armv2', 'armv1', 'armv0', 'armv', 'arm64', 'armle', 'armbe', 'armhf', 'armel', 'arm64', 'armv7', 'armv8', 'armv6', 'armv5', 'armv4', 'armv3', 'armv2', 'armv1', 'armv0', 'armv', 'arm64', 'armle', 'armbe', 'armhf', 'armel', 'arm64', 'armv7', 'armv8', 'armv6', 'armv5', 'armv4', 'armv3', 'armv2', 'armv1', 'armv0', 'armv']
+@click.group()
+def cli():
+    pass
+def generate_self_signed_cert(cert_dir):
+    if not os.path.exists(cert_dir):
+        os.makedirs(cert_dir)
+
+    cert_path = os.path.join(cert_dir, 'cert.pem')
+    key_path = os.path.join(cert_dir, 'key.pem')
+
+    if not os.path.exists(cert_path) or not os.path.exists(key_path):
+        key = crypto.PKey()
+        key.generate_key(crypto.TYPE_RSA, 2048)
+
+        cert = crypto.X509()
+        cert.get_subject().C = "US"
+        cert.get_subject().ST = "CA"
+        cert.get_subject().L = "San Francisco"
+        cert.get_subject().O = "qu1ckdr0p"
+        cert.get_subject().OU = "qu1ckdr0p"
+        cert.get_subject().CN = "byinarie@localhost"
+        cert.set_serial_number(1000)
+        cert.gmtime_adj_notBefore(0)
+        cert.gmtime_adj_notAfter(315360000)  # Valid for 10 years
+        cert.set_issuer(cert.get_subject())
+        cert.set_pubkey(key)
+        cert.sign(key, 'sha256')
+
+        with open(cert_path, 'wb') as cert_file:
+            cert_file.write(crypto.dump_certificate(crypto.FILETYPE_PEM, cert))
+
+        with open(key_path, 'wb') as key_file:
+            key_file.write(crypto.dump_privatekey(crypto.FILETYPE_PEM, key))
+
+        click.echo(click.style(f"[+] Certificate and key generated at {cert_path} and {key_path}", fg='green'))
+
+    return cert_path, key_path
+
+def create_directories():
+    directories = ["downloads", "windows", "linux", "mac"]
+    for directory in directories:
+        if not os.path.exists(directory):
+            os.makedirs(directory)
+            
+def get_interface_ip(interface):
+    try:
+        ip = ni.ifaddresses(interface)[ni.AF_INET][0]['addr']
+        return ip
+    except Exception as e:
+        return None
+
+def get_serving_ip():
+    tun0_ip = get_interface_ip('tun0')
+    if tun0_ip:
+        return tun0_ip
+    eth0_ip = get_interface_ip('eth0')
+    if eth0_ip:
+        return eth0_ip
+    return '0.0.0.0'
+
+
+
+
+
+
+
+def extract_archive(archive_path, download_directory):
+    file_list = []
+
+    for root, _, files in os.walk(download_directory):
+        for file in files:
+            file_extension = os.path.splitext(file)[1]
+            if file_extension in ['.zip', '.gz', '.tar.gz', '.tgz', '.tar.bz2', '.tbz2', '.tbz', '.tar.xz', '.txz', '.tar']:
+                file_list.append(os.path.join(root, file))
+
+    for file in tqdm(file_list, desc='Extracting archives', unit='archive'):
+        try:
+            filename = os.path.basename(file)
+            file_extension = os.path.splitext(filename)[-1]
+            archive_path = os.path.join(root, file)
+
+            if file_extension == '.zip':
+                with zipfile.ZipFile(archive_path, 'r') as zip_ref:
+                    zip_ref.extractall(download_directory)
+
+                    for subdir, _, files in os.walk(download_directory):
+                        for sub_file in files:
+                            if subdir != download_directory:  # Avoid moving files that are already in the target directory, eventually this should be removed
+                                src_path = os.path.join(subdir, sub_file)
+                                dest_path = os.path.join(download_directory, sub_file)
+                                shutil.move(src_path, dest_path)
+
+                    for subdir in [d for d in os.listdir(download_directory) if os.path.isdir(os.path.join(download_directory, d))]:
+                        shutil.rmtree(os.path.join(download_directory, subdir))
+            elif file_extension in ['.tar.gz', '.tgz', '.tar.bz2', '.tbz2', '.tbz', '.tar.xz', '.txz', '.tar']:
+                os.system(f"tar -xvf {archive_path} -C {download_directory}")
+            elif file_extension == '.gz':
+                os.system(f"gunzip -d {archive_path} {download_directory}")
+        except Exception as e:
+            click.echo(click.style(f"[-] Failed to extract {filename}", fg='red'))
+        finally:
+            click.echo(click.style(f"[+] Extracted {filename} to {download_directory}", fg='green'))
+
+            
+def process_file(file_path):
+    files = os.listdir(file_path)
+    linux = os.getcwd() + "/linux"
+    mac = os.getcwd() + "/mac"
+    windows = os.getcwd() + "/windows"
+    blacklist_keywords = ["readme.md", "license", "file hash.txt"]  
+
+    for filename in tqdm(files, desc='Processing files', unit='file'):
+        if filename.lower() in [name.lower() for name in blacklist_keywords]:
+            continue
+        
+        file_full_path = os.path.join(file_path, filename)
+        try:
+            file_type = subprocess.check_output(["file", "--brief", file_full_path]).decode().strip()
+            
+            if "executable" in file_type and "ELF" in file_type:
+                shutil.copy(file_full_path, linux)
+            elif "Mach-O" in file_type:
+                shutil.copy(file_full_path, mac)
+            elif "MS-DOS executable" in file_type or "PE32" in file_type:
+                shutil.copy(file_full_path, windows)
+            elif "Bourne-Again shell script" in file_type or "POSIX shell script" in file_type:
+                shutil.copy(file_full_path, linux)
+            elif "ASCII text" in file_type:
+                shutil.copy(file_full_path, windows)
+        except subprocess.CalledProcessError:
+            click.echo(click.style(f"[-] Failed to process {filename}", fg='red'))
+
+def download_latest_release(tool_name, release_url, download_dir):
+    config = configparser.ConfigParser()
+    config.read('config/settings.ini')
+    api_key = config['GitHub']['API_KEY']
+    headers = {'Authorization': f'token {api_key}'}
+    user, repo = release_url.split('/')[3:5]
+    api_url = f'https://api.github.com/repos/{user}/{repo}/releases/latest'
+    response = requests.get(api_url, headers=headers)
+    if response.status_code != 200:
+        click.echo(click.style(f"[-] Failed to fetch the latest release for {tool_name}", fg='red'))
+        return
+
+    assets = response.json().get('assets', [])
+    for asset in assets:
+        if 'source' in asset['name'] or any(keyword in asset['name'] for keyword in blacklist_keywords):
+            continue
+
+        download_url = asset['browser_download_url']
+        response = requests.get(download_url, stream=True)
+        file_path = os.path.join(download_dir, asset['name'])
+        with open(file_path, 'wb') as file:
+            shutil.copyfileobj(response.raw, file)
+        click.echo(click.style(f"[+] Downloaded {tool_name} to {file_path}", fg='green'))
+        
+def download_latest_releases(download_dir):
+    config = configparser.ConfigParser()
+    config.read('config/repos.ini')
+    repos = list(config.items('REPOS'))
+    
+    # print("Repos:", repos) for debugging
+
+    with tqdm(repos, desc="Downloading repositories", unit="repo") as t:
+        for tool_name, release_url in t:
+            download_latest_release(tool_name, release_url, download_dir)     
+             
+def update_repositories(file_path, download_dir):
+    create_directories()
+    script_dir = os.path.dirname(os.path.abspath(__file__))
+    downloads_dir = os.path.join(script_dir, 'downloads')
+    download_latest_releases(downloads_dir)
+
+    extract_archive(file_path, download_directory)
+
+    process_file(file_path)
+
+
+
+@cli.command()
+def update():
+    """Update tools in linux/ mac/ and windows/ (messy but works)"""
+    update_repositories(download_directory, target_directory)
+
+
+@cli.command()
+@click.argument('alias', type=str, required=False)
+@click.option('-d', '--directory', type=click.Path(exists=True, file_okay=False), help='Path to the directory to serve.')
+@click.option('-f', '--file', type=click.Path(exists=True, dir_okay=False), help='Path to the file to serve.')
+@click.option('--https', is_flag=True, help='Use HTTPS instead of HTTP.')
+@click.option('--port', default=80, help='Port number to run the server on.')
+def serve(alias=None, directory=None, file=None, https=False, port=8080):
+    """Serve your bin: serv.py $alias, serv.py -d $dir, or serv.py -f $file"""
+
+    script_dir = os.path.dirname(os.path.abspath(__file__))
+    common_ini_path = os.path.join(script_dir, 'config/common.ini')
+    config.read(common_ini_path)
+    if directory and file:
+        click.echo(click.style(f"[-] Please provide either a directory path or a file path, not both.", fg='red'))
+        return
+
+    if https:
+        cert_dir = 'certs/'
+        cert_path, key_path = generate_self_signed_cert(cert_dir)
+        ssl_context = (cert_path, key_path)
+    else:
+        ssl_context = None
+
+    if directory:
+        path_to_serve = directory
+    elif file:
+        path_to_serve = file
+    elif alias:
+        dir_section = None
+        for section in config.sections():
+            if alias in config[section]:
+                dir_section = section
+                break
+
+        if dir_section:
+            relative_path = config.get(dir_section, alias, fallback=None)
+            path_to_serve = os.path.join(script_dir, relative_path)
+        else:
+            click.echo(click.style(f"[-] Alias '{alias}' not found in config/common.ini.", fg='red'))
+            return
+    else:
+        path_to_serve = os.getcwd()
+
+    ip_address = get_serving_ip()
+    protocol = 'https' if https else 'http'
+    app = Flask(__name__)
+
+    if os.path.isdir(path_to_serve):
+        click.echo(click.style(f"[+] Serving at {protocol}://{ip_address}:{port}/", fg='green'))
+        for filename in os.listdir(path_to_serve):
+            click.echo(click.style(f"{protocol}://{ip_address}:{port}/{filename}", fg='green'))
+    else:
+        filename = os.path.basename(path_to_serve)
+        click.echo(click.style(f"[+] Serving at {protocol}://{ip_address}:{port}/{filename}", fg='green'))
+
+    @app.route('/<path:filename>')
+    def serve_directory(filename):
+        if os.path.isdir(path_to_serve):
+            full_path = os.path.join(path_to_serve, filename)
+        else:
+            full_path = path_to_serve if filename == os.path.basename(path_to_serve) else None
+
+        if full_path and os.path.exists(full_path):
+            return send_from_directory(os.path.dirname(full_path), os.path.basename(full_path))
+        return "File not found", 404
+
+    app.run(host=ip_address, port=port, ssl_context=ssl_context)
+
+def display_aliases(search=None):
+    config = configparser.ConfigParser()
+    config.read('config/common.ini')  
+
+    for section in config.sections():
+        for key, value in config.items(section):
+            alias_name = key
+            alias_path = value
+            if search and (search.lower() not in alias_name.lower() and search.lower() not in alias_path.lower()):
+                continue
+            click.echo(click.style(f"Alias: {alias_name}\nPath: {alias_path}\n", fg='green'))
+
+@cli.command(name="list")
+@click.option('--search', type=str, help='Search for a specific alias using a search string.')
+def list_aliases(search):
+    """List all aliases or search for a specific alias."""
+    display_aliases(search)  
+
+
+    
+if __name__ == "__main__":
+    download_directory = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'downloads/')
+    target_directory = os.path.dirname(os.path.abspath(__file__))   
+    cli()
