9d593c76d0f7cf2af02899fe53cb28bb83c94c4a
byinarie <byinarie@gmail.com>
2023-09-01 19:12:12 -0500

    updating common.init

--- qu1ckdr0p2/serv.py
+++ qu1ckdr0p2/serv.py
@@ -8,13 +8,26 @@ import logging
 import click
 import configparser
 import requests
-from concurrent.futures import ThreadPoolExecutor
+import logging
 
+from concurrent.futures import ThreadPoolExecutor
 
 log = logging.getLogger('werkzeug')
 log.setLevel(logging.ERROR)
+
 user_home = os.path.expanduser("~")
 base_dir = os.path.join(user_home, ".qu1ckdr0p2")
+directory = os.path.dirname(os.path.abspath(__file__))
+
+target_directories = ["config", "windows", "linux", "mac"]
+other_directories = ["payloads", "certs"]
+
+payloads = os.path.join(user_home, ".qu1ckdr0p2", 'payloads')
+cert_dir = os.path.join(user_home, ".qu1ckdr0p2", 'certs')
+
+cert_path = os.path.join(cert_dir, 'cert.pem')
+key_path = os.path.join(cert_dir, 'key.pem')
+
 
 config = configparser.ConfigParser()
 common_ini_path = os.path.join(base_dir, 'config/common.ini')
@@ -22,17 +35,12 @@ config.read(common_ini_path)
 # print(config.sections())
 # print(config.items("._DIR"))
 # Not sorting out other platforms for now
-blacklist_keywords = ['sample', 'arm64', 'readme.md', 'readme', 'license', 'mips', 'mips64', 'mipsle', 'ppc', 's390', 'arm', 'aarch64', 'armv7', 'armv8', 'armv6', 'armv5', 'armv4', 'armv3', 'armv2', 'armv1', 'armv0', 'armv', 'arm64', 'armle', 'armbe', 'armhf', 'armel', 'arm64', 'armv7', 'armv8', 'armv6', 'armv5', 'armv4', 'armv3', 'armv2', 'armv1', 'armv0', 'armv', 'arm64', 'armle', 'armbe', 'armhf', 'armel', 'arm64', 'armv7', 'armv8', 'armv6', 'armv5', 'armv4', 'armv3', 'armv2', 'armv1', 'armv0', 'armv', 'arm64', 'armle', 'armbe', 'armhf', 'armel', 'arm64', 'armv7', 'armv8', 'armv6', 'armv5', 'armv4', 'armv3', 'armv2', 'armv1', 'armv0', 'armv', 'arm64', 'armle', 'armbe', 'armhf', 'armel', 'arm64', 'armv7', 'armv8', 'armv6', 'armv5', 'armv4', 'armv3', 'armv2', 'armv1', 'armv0', 'armv', 'arm64', 'armle', 'armbe', 'armhf', 'armel', 'arm64', 'armv7', 'armv8', 'armv6', 'armv5', 'armv4', 'armv3', 'armv2', 'armv1', 'armv0', 'armv', 'arm64', 'armle', 'armbe', 'armhf', 'armel', 'arm64', 'armv7', 'armv8', 'armv6', 'armv5', 'armv4', 'armv3', 'armv2', 'armv1', 'armv0', 'armv']
 @click.group()
 def cli():
     pass
 
 def generate_self_signed_cert(cert_dir):
-    if not os.path.exists(cert_dir):
-        os.makedirs(cert_dir)
 
-    cert_path = os.path.join(cert_dir, 'cert.pem')
-    key_path = os.path.join(cert_dir, 'key.pem')
 
     if not os.path.exists(cert_path) or not os.path.exists(key_path):
         key = crypto.PKey()
@@ -78,25 +86,33 @@ def get_serving_ip():
         return eth0_ip
     return '0.0.0.0'
 
+
+
+
 @cli.command()
 @click.argument('alias', type=str, required=False)
-@click.option('-d', '--directory', type=click.Path(exists=True, file_okay=False), help='Path to the directory to serve.')
-@click.option('-f', '--file', type=click.Path(exists=True, dir_okay=False), help='Path to the file to serve.')
-@click.option('--https', is_flag=True, help='Use HTTPS instead of HTTP.')
-@click.option('--port', default=80, help='Port number to run the server on.')
-def serve(alias=None, directory=None, file=None, https=False, port=8080):
+@click.option('-d', '--directory', type=click.Path(exists=True, file_okay=False))
+@click.option('-f', '--file', type=click.Path(exists=True, dir_okay=False))
+@click.option('--https', default=True, is_flag=True)
+@click.option('-p', '--port', default=443)
+@click.option('-u', '--use', type=int)
+def serve(alias=None, directory=None, file=None, https=False, port=443, use=None):
     global base_dir  # Make sure to use the global base_dir variable
+
+    if use and alias:
+        alias = f"{alias}{use}"
 
     if directory and file:
         click.echo("Please provide either a directory path or a file path, not both.")
         return
 
+    ssl_context = None
     if https:
         cert_dir = 'certs/'
+        if not os.path.exists(cert_dir):
+            os.makedirs(cert_dir)
         cert_path, key_path = generate_self_signed_cert(cert_dir)
         ssl_context = (cert_path, key_path)
-    else:
-        ssl_context = None
 
     if directory:
         path_to_serve = directory
@@ -111,16 +127,20 @@ def serve(alias=None, directory=None, file=None, https=False, port=8080):
 
         if dir_section:
             relative_path = config.get(dir_section, alias, fallback=None)
-            path_to_serve = os.path.join(base_dir, relative_path)  # Use base_dir here
+            path_to_serve = os.path.join(base_dir, relative_path)
         else:
             click.echo(f"Alias '{alias}' not found in config/common.ini.")
             return
     else:
-        path_to_serve = base_dir 
+        path_to_serve = base_dir
 
+    serve_files(path_to_serve, https=https, port=port, ssl_context=ssl_context)
+
+def serve_files(path_to_serve, https=True, port=443, ssl_context=None):
     ip_address = get_serving_ip()
     protocol = 'https' if https else 'http'
     app = Flask(__name__)
+
     if os.path.isdir(path_to_serve):
         click.echo(f"Serving at {protocol}://{ip_address}:{port}/")
         for filename in os.listdir(path_to_serve):
@@ -138,22 +158,64 @@ def serve(alias=None, directory=None, file=None, https=False, port=8080):
         if full_path and os.path.exists(full_path):
             return send_from_directory(os.path.dirname(full_path), os.path.basename(full_path))
         return "File not found", 404
-    app.run(host=ip_address, port=port, ssl_context=ssl_context)
 
+    app.run(host='0.0.0.0', port=port, ssl_context=ssl_context)
+    
 @cli.command(name="list")
-@click.option('--search', type=str, help='Search for a specific alias using a search string.')
-def list_aliases(search):
-    """List all aliases or search for a specific alias."""
-    display_aliases(search)  
+@click.option('--search', type=str)
+@click.option('-u', '--use', type=int)
+@click.option('--port', default=443)
+def list_aliases(search, use, port):
+    if use:
+        invoke_serve_by_number(search, use, port=port)
+    else:
+        display_aliases(search)
 
-def display_aliases(search=None):
+def invoke_serve_by_number(search=None, use=None, port=443):
+    counter = 1
+    selected_alias = None
     for section in config.sections():
         for key, value in config.items(section):
             alias_name = key
             alias_path = os.path.join(base_dir, value)
-            if search and (search.lower() not in alias_name.lower() and search.lower() not in alias_path.lower()):
-                continue
-            click.echo(f"Alias: {alias_name}\nPath: {alias_path}\n")
+            if search:
+                search_lower = search.lower()
+                if not (search_lower in alias_name.lower() or search_lower in alias_path.lower()):
+                    continue
+            if counter == use:
+                selected_alias = alias_name
+                break
+            counter += 1
+    if selected_alias:
+        dir_section = None
+        for section in config.sections():
+            if selected_alias in config[section]:
+                dir_section = section
+                break
+        if dir_section:
+            relative_path = config.get(dir_section, selected_alias, fallback=None)
+            path_to_serve = os.path.join(base_dir, relative_path)
+        else:
+            click.echo(f"Alias '{selected_alias}' not found in config/common.ini.")
+            return
+        serve_files(path_to_serve, port=port)
+
+
+    else:
+        click.echo(f"No alias found for the number {use}.")
+
+def display_aliases(search=None):
+    counter = 1
+    for section in config.sections():
+        for key, value in config.items(section):
+            alias_name = key
+            alias_path = os.path.join(base_dir, value)
+            if search:
+                search_lower = search.lower()
+                if not (search_lower in alias_name.lower() or search_lower in alias_path.lower()):
+                    continue
+            click.echo(f"Alias: {alias_name}\nPath: {alias_path}\nuse: {counter}^\n-> -u {alias_name}\n")
+            counter += 1
             
                         
 @cli.command()
@@ -164,77 +226,81 @@ def display_aliases(search=None):
 @click.option('--skip-mac', is_flag=True, help='Skip checking the mac directory.')
 @click.option('--api-key', help='GitHub API key for authentication')
 def init(check, skip_config, skip_windows, skip_linux, skip_mac, api_key):
-    user_home = os.path.expanduser("~")
-    base_dir = os.path.join(user_home, ".qu1ckdr0p2")
-    directory = os.path.dirname(os.path.abspath(__file__))
-    target_directories = ["config", "windows", "linux", "mac"]
+    create_directory(base_dir)
+    
+    skip_directories = []
+    if skip_config: skip_directories.append("config")
+    if skip_windows: skip_directories.append("windows")
+    if skip_linux: skip_directories.append("linux")
+    if skip_mac: skip_directories.append("mac")
 
-    if not os.path.exists(base_dir):
-        os.makedirs(base_dir)
-
-    for dir_name in target_directories:
-        dir_path = os.path.join(base_dir, dir_name)
-        if not os.path.exists(dir_path):
-            os.makedirs(dir_path)
-
+    all_directories = target_directories + other_directories
+    for dir_name in all_directories:
+        if dir_name in skip_directories:
+            continue
+        full_path = os.path.join(base_dir, dir_name)
+        create_directory(full_path)
+    
     click.echo(click.style(f"[+] Directories exist. Continuing.", fg='green'))
     
-    
-    for target_dir in target_directories:
-        full_path = os.path.join(base_dir, target_dir)
-        print(full_path)
-    
-    repo_owner = "byinarie"
-    repo_name = "qu1ckdr0p2"
-    
     headers = {'Authorization': f'token {api_key}'} if api_key else None
+    handle_github_auth(api_key)
+    
+    for directory in target_directories:
+        if directory in skip_directories:
+            continue
+        handle_directory(directory, headers, check)
+
+def create_directory(dir_path):
+    if not os.path.exists(dir_path):
+        os.makedirs(dir_path)
+        click.echo(click.style(f"[+] Created directory: {dir_path}", fg='green'))
+    else:
+        click.echo(click.style(f"[=] Directory already exists: {dir_path}", fg='yellow'))
+
+
+def handle_github_auth(api_key):
     if api_key:
         click.echo(click.style(f"[+] Using GitHub API key for authentication", fg='green'))
     else:
         click.echo(click.style(f"[-] No GitHub API key provided, using unauthenticated requests", fg='yellow'))
-        click.echo(click.style(f"[-] Anauthenticated requests are subject to higher limiting", fg='yellow')) 
-        click.echo(click.style(f"[-] You can create a token here: https://github.com/settings/tokens/new", fg='yellow')) 
-               
-    for directory in target_directories:
-        if (skip_config and directory == 'config') or (skip_windows and directory == 'windows') or (skip_linux and directory == 'linux') or (skip_mac and directory == 'mac'):
-            continue
-        
-        target_path = os.path.join(base_dir, directory)
-        target_url = f"https://api.github.com/repos/{repo_owner}/{repo_name}/contents/qu1ckdr0p2/{directory}"
-            
-        response = requests.get(target_url, headers=headers)
-        if response.status_code != 200:
-            click.echo(click.style(f"[-] Failed to fetch directory contents: {target_url}. Status Code: {response.status_code}, Reason: {response.reason}", fg='red'))
-            continue
+        click.echo(click.style(f"[-] Unauthenticated requests are subject to higher limiting", fg='yellow')) 
+        click.echo(click.style(f"[-] You can create a token here: https://github.com/settings/tokens/new", fg='yellow'))
 
-        files = response.json()
-        local_files = list_local_files(target_path)
-        for file_info in files:
-            if file_info['type'] != 'file':
-                continue
+def handle_directory(directory, headers, check):
+    target_path = os.path.join(base_dir, directory)
+    target_url = f"https://api.github.com/repos/byinarie/qu1ckdr0p2/contents/qu1ckdr0p2/{directory}"
             
-            file_name = file_info['name']
-            file_url = file_info.get('download_url')
-            if not file_url:
-                click.echo(click.style(f"[-] Download URL not found for {file_name}", fg='red'))
-                continue
-            
-            file_path = os.path.join(target_path, file_name)
-            
-            if check:
-                if file_name not in local_files:
-                    download_and_save_file(file_url, file_path)
-                else:
-                    with open(file_path, 'rb') as f:
-                        existing_content = f.read()
-                    existing_sha1 = calculate_git_blob_sha1(existing_content)
-                    expected_sha1 = file_info.get('sha')
+    response = requests.get(target_url, headers=headers)
+    if response.status_code != 200:
+        click.echo(click.style(f"[-] Failed to fetch directory contents: {target_url}. Status Code: {response.status_code}, Reason: {response.reason}", fg='red'))
+        return
 
-                    if existing_sha1 != expected_sha1:
-                        download_and_save_file(file_url, file_path)
-                        click.echo(click.style(f"[+] Updated {file_path}", fg='green'))
-            else:
+    files = response.json()
+    local_files = list_local_files(target_path)
+    
+    for file_info in files:
+        if file_info['type'] != 'file':
+            continue
+        file_name = file_info['name']
+        file_url = file_info.get('download_url')
+        if not file_url:
+            click.echo(click.style(f"[-] Download URL not found for {file_name}", fg='red'))
+            continue
+        file_path = os.path.join(target_path, file_name)
+        if check:
+            if file_name not in local_files:
                 download_and_save_file(file_url, file_path)
+            else:
+                with open(file_path, 'rb') as f:
+                    existing_content = f.read()
+                existing_sha1 = calculate_git_blob_sha1(existing_content)
+                expected_sha1 = file_info.get('sha')
+                if existing_sha1 != expected_sha1:
+                    download_and_save_file(file_url, file_path)
+                    click.echo(click.style(f"[+] Updated {file_path}", fg='green'))
+        else:
+            download_and_save_file(file_url, file_path)
                 
 def download_and_save_file(url, file_path):
     response = requests.get(url)
@@ -254,6 +320,5 @@ def list_local_files(directory_path):
     return set(os.listdir(directory_path))
 
 if __name__ == "__main__":
-    download_directory = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'downloads/')
     target_directory = os.path.dirname(os.path.abspath(__file__))   
     cli()
